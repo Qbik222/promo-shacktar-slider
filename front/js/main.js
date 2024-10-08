687e790522cc95ab21ee4c36b8cc2cfe9aea949d
yurii <ignatovyw@gmail.com>
2024-10-08 18:27:49 +0300

    desc done

---
+++ front/js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    // popups
+    function setPopups(popups, closeBtns, showBtns){
+        popups = document.querySelectorAll(`${popups}`)
+        closeBtns = document.querySelectorAll(`${closeBtns}`)
+        showBtns = document.querySelectorAll(`${showBtns}`)
+
+        showBtns.forEach((showBtn, showBtnIndex) => {
+            showBtn.addEventListener("click", () =>{
+                popups.forEach((popup, popupIndex) =>{
+                    if(popupIndex === showBtnIndex){
+                        popup.classList.toggle("active")
+                    }else{
+                        popup.classList.remove("active")
+                    }
+                })
+            })
+        })
+        closeBtns.forEach((showBtn, closeBtnIndex) => {
+            showBtn.addEventListener("click", () =>{
+                popups.forEach((popup, popupIndex) =>{
+                    if(popupIndex === closeBtnIndex){
+                        popup.classList.remove("active")
+                    }
+                })
+            })
+        })
+
+    }
+
+    setPopups(".guide__list-popup", ".guide__list-popup-close", ".guide__list-btn")
+
+    // score counter
+    let team1 = 0;
+    let team2 = 0;
+
+    function setScore(score, scoreContainer, decreaseBtn, increaseBtn){
+        scoreContainer = document.querySelector(`${scoreContainer}`)
+        decreaseBtn = document.querySelector(`${decreaseBtn}`)
+        increaseBtn = document.querySelector(`${increaseBtn}`)
+
+        scoreContainer.innerText = score
+        increaseBtn.addEventListener("click", () =>{
+            ++score
+            scoreContainer.innerText = score
+        })
+        decreaseBtn.addEventListener("click", () =>{
+            if(score === 0) return
+            --score
+            scoreContainer.innerText = score
+        })
+    }
+
+    setScore(team1, ".predict__score-team1", ".predict__team1-decrease", ".predict__team1-increase")
+    setScore(team2, ".predict__score-team2", ".predict__team2-decrease", ".predict__team2-increase")
+})
