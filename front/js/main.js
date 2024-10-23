43d3b17ab067a06f9e7b91294ff2749c19afdf1e
yurii <ignatovyw@gmail.com>
2024-10-23 13:53:27 +0300

    slider add

--- front/js/main.js
+++ front/js/main.js
@@ -199,26 +199,61 @@
         }
     }
 
-    const scorePrediction = {team1 : 0, team2: 0}
+    const scorePrediction = [
+        {team1 : 0, team2: 0},
+        {team1 : 0, team2: 0},
+        {team1 : 0, team2: 0},
+        {team1 : 0, team2: 0},
+        {team1 : 0, team2: 0},
+    ]
+
+    function setScore(teamNumber){
+        const scorePanels = document.querySelectorAll(`.predictionTeam${teamNumber}`);
+
+        const fieldTag = `team${teamNumber}`;
+
+        scorePanels.forEach((panel, i) =>{
+            panel.innerHTML = scorePrediction[i][fieldTag];
+        })
+    }
 
     function initScoreSelector(teamNumber) {
-        const minusBtn = document.querySelector(`.team${teamNumber}-minus`);
-        const plusBtn = document.querySelector(`.team${teamNumber}-plus`);
-        const scorePanel = document.querySelector(`.predictionTeam${teamNumber}`);
+        const minusBtns = document.querySelectorAll(`.team${teamNumber}-minus`);
+        const plusBtns = document.querySelectorAll(`.team${teamNumber}-plus`);
+        const scorePanels = document.querySelectorAll(`.predictionTeam${teamNumber}`);
 
-        minusBtn.addEventListener('click', () => {
-            const fieldTag = `team${teamNumber}`;
-            const currentScore = scorePrediction[fieldTag];
-            scorePrediction[fieldTag] = Math.max(currentScore - 1, 0);
-            scorePanel.innerHTML = scorePrediction[fieldTag];
-        });
+        const fieldTag = `team${teamNumber}`;
 
-        plusBtn.addEventListener('click', () => {
-            const fieldTag = `team${teamNumber}`;
-            const currentScore = scorePrediction[fieldTag];
-            scorePrediction[fieldTag] = Math.min(currentScore + 1, 99);
-            scorePanel.innerHTML = scorePrediction[fieldTag];
-        });
+        scorePanels.forEach((panel, i) =>{
+            panel.innerHTML = scorePrediction[i][fieldTag];
+        })
+
+
+        minusBtns.forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+
+                const currentScore = scorePrediction[i][fieldTag];
+                console.log(scorePrediction)
+                scorePrediction[i][fieldTag] = Math.max(currentScore - 1, 0);
+                scorePanels.forEach(panel =>{
+                    panel.innerHTML = scorePrediction[i][fieldTag];
+                })
+
+            });
+        })
+
+
+        plusBtns.forEach((btn, i) =>{
+            btn.addEventListener('click', () => {
+                // const fieldTag = `team${teamNumber}`;
+                const currentScore = scorePrediction[i][fieldTag];
+                scorePrediction[i][fieldTag] = Math.min(currentScore + 1, 99);
+                scorePanels.forEach(panel =>{
+                    panel.innerHTML = scorePrediction[i][fieldTag];
+                })
+            });
+        })
+
     }
 
     let isRequestInProgress;
@@ -334,5 +369,197 @@
     titles.forEach(title =>{
         animateOnScroll(title, "fadeIn")
     })
+
+
+
+    /// гліч слайдер
+    function createSlider(slides, leftBtn, rightBtn, slidesIcons, current, path, img, week, coverflow, coverflowOffWidth, subtitles, copySlides){
+        let coverflowToggler = true
+        if(window.innerWidth < coverflowOffWidth){
+            coverflowToggler = false
+        }
+        function coverFlowClasses(right, left, slides) {
+            slides.forEach((slide, i) => {
+                if (coverflowToggler) {
+                    if (current === i) {
+                        let prevIndex = (i - 1 + slides.length) % slides.length;
+                        slides[prevIndex].classList.add(right);
+                        let nextIndex = (i + 1) % slides.length;
+                        slides[nextIndex].classList.add(left);
+                    }
+                }
+            });
+        }
+
+        slides = document.querySelectorAll(slides);
+        subtitles = document.querySelectorAll(subtitles);
+        leftBtn = document.querySelector(leftBtn);
+        rightBtn = document.querySelector(rightBtn);
+        slidesIcons = document.querySelectorAll(slidesIcons);
+        let glitchLayers = [];
+        slides.forEach(slide => {
+            glitchLayers = [...glitchLayers, ...slide.querySelectorAll(".glitch__layer")];
+        });
+        if(slides[current])slides[current].classList.add("_active");
+        if(coverflow){
+            coverFlowClasses("right-cover", "left-cover", slides)
+        }
+
+        // function subtitlesInit(subtitles, slides){
+        //     // console.log(slides)
+        //     slides.forEach((slide, slideIndex) =>{
+        //         if(slide.classList.contains("_active")){
+        //             subtitles.forEach((subtitle, subtitleIndex) =>{
+        //                 subtitle.classList.remove("_active")
+        //                 if(slideIndex === subtitleIndex){
+        //                     subtitle.classList.add("_active")
+        //                 }
+        //             })
+        //         }
+        //
+        //     })
+        // }
+        function moveSlider(slides, direction) {
+            if (direction === "left") {
+                --current;
+                if (current < 0) current = slides.length - 1;
+            } else if (direction === "right") {
+                ++current;
+                if (current > slides.length - 1) current = 0;
+            }
+
+            slides.forEach((slide, i) => {
+                slide.classList.toggle("_active", i === current);
+                slide.querySelector(".predict__team1").classList.remove("left-anim");
+                slide.querySelector(".predict__team2").classList.remove("right-anim");
+            });
+
+            SlideIconsInit(slidesIcons, current);
+        }
+
+        function SlideIconsInit(icons, current) {
+            const wrapper = icons[0].parentElement.parentElement;
+            // console.log(wrapper)
+
+            icons.forEach((icon, iconIndex) => {
+                icon.classList.toggle("_current", current === iconIndex);
+                if (current === iconIndex) {
+                    const iconOffsetLeft = icon.offsetLeft;
+                    const iconWidth = icon.offsetWidth;
+                    const wrapperWidth = wrapper.offsetWidth;
+                    wrapper.scrollTo({
+                        left: iconOffsetLeft - (wrapperWidth / 2) + (iconWidth / 2),
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        }
+
+        function handleClick(direction) {
+            slides.forEach(slide =>{
+                const leftCard = slide.querySelector(".predict__team1")
+                const rightCard = slide.querySelector(".predict__team2")
+                const leftCardBack = leftCard.querySelector(".back-img")
+                const rightCardBack = rightCard.querySelector(".back-img")
+                leftCard.classList.add("left-anim");
+                rightCard.classList.add("right-anim");
+                console.log( current - 1, slides.length)
+                if(current + 1 < slides.length && direction === "right"){
+                    leftCardBack.style.background = `url("../img/predict/left-team-desc${current + 2}.png") no-repeat 0 0/contain`
+                    rightCardBack.style.background = `url("../img/predict/right-team-desc${current + 2}.png") no-repeat 0 0/contain`
+
+                }else if (current + 1 === slides.length && direction === "right"){
+                    leftCardBack.style.background = `url("../img/predict/left-team-desc1.png") no-repeat 0 0/contain`
+                    rightCardBack.style.background = `url("../img/predict/right-team-desc1.png") no-repeat 0 0/contain`
+                }
+                if(current - 1 > 1 && direction === "left"){
+                    leftCardBack.style.background = `url("../img/predict/left-team-desc${current }.png") no-repeat 0 0/contain`
+                    rightCardBack.style.background = `url("../img/predict/right-team-desc${current }.png") no-repeat 0 0/contain`
+
+                }else if (current - 1 === 0 && direction === "left"){
+                    leftCardBack.style.background = `url("../img/predict/left-team-desc${slides.length}.png") no-repeat 0 0/contain`
+                    rightCardBack.style.background = `url("../img/predict/right-team-desc${slides.length}.png") no-repeat 0 0/contain`
+                }
+
+            })
+            rightBtn.style.pointerEvents = "none";
+            leftBtn.style.pointerEvents = "none";
+            setTimeout(() => {
+                moveSlider(slides, direction);
+                rightBtn.style.pointerEvents = "initial";
+                leftBtn.style.pointerEvents = "initial";
+                setScore(1)
+                setScore(2)
+                // initScoreSelector(1);
+                // initScoreSelector(2);
+                if(coverflow){
+                    slides.forEach(slide =>{
+                        slide.classList.remove("right-cover")
+                        slide.classList.remove("left-cover")
+                        slide.classList.remove("glitch")
+                    })
+                    coverFlowClasses("right-cover", "left-cover", slides)
+
+                }
+            }, 1000);
+        }
+
+        leftBtn.addEventListener("click", () => handleClick("left"));
+        rightBtn.addEventListener("click", () => handleClick("right"));
+
+        slidesIcons.forEach((icon, i) => {
+            icon.addEventListener("click", (e) => {
+                if(e.target.classList.contains("_current")) return
+                setTimeout(() => {
+                    slidesIcons.forEach(item => item.classList.remove("_current"));
+                }, 1000);
+                slidesIcons.forEach(icon =>{
+                    icon.style.pointerEvents = "none"
+
+                })
+                rightBtn.style.pointerEvents = "none";
+                leftBtn.style.pointerEvents = "none";
+
+                slides[current].classList.add("glitch");
+                current = i;
+                slides.forEach(slide =>{
+                    const leftCard = slide.querySelector(".predict__team1")
+                    const rightCard = slide.querySelector(".predict__team2")
+                    const leftCardBack = leftCard.querySelector(".back-img")
+                    const rightCardBack = rightCard.querySelector(".back-img")
+                    leftCard.classList.add("left-anim");
+                    rightCard.classList.add("right-anim");
+                    leftCardBack.style.background = `url("../img/predict/left-team-desc${current + 1}.png") no-repeat 0 0/contain`
+                    rightCardBack.style.background = `url("../img/predict/right-team-desc${current + 1}.png") no-repeat 0 0/contain`
+                })
+                setTimeout(() => {
+                    SlideIconsInit(slidesIcons, current);
+                    slides.forEach((slide, index) => {
+                        slide.classList.toggle("_active", index === current);
+                        const leftCard = slide.querySelector(".predict__team1")
+                        const rightCard = slide.querySelector(".predict__team2")
+                        leftCard.classList.remove("left-anim");
+                        rightCard.classList.remove("right-anim");
+                        // slide.classList.remove("glitch");
+                        // subtitlesInit(subtitles, slides)
+                    });
+                    rightBtn.style.pointerEvents = "initial";
+                    leftBtn.style.pointerEvents = "initial";
+                    slidesIcons.forEach(icon =>{
+                        icon.style.pointerEvents = "initial"
+
+                    })
+                    setScore(1)
+                    setScore(2)
+
+                }, 1000);
+            });
+        });
+        SlideIconsInit(slidesIcons, current);
+        // subtitlesInit(subtitles, slides)
+
+    }
+
+    createSlider(".predict__counter", ".predict__move-left", ".predict__move-right", ".predict__icons-item", 1, null, "pers.png", null, false, null, null, true)
 
 })();
